ab32b6ed5b816ae39200a5d45f931d899d9e24d8
Arthur Coelho <arthurclho@gmail.com>
2026-05-21 13:01:17 -0300

    Initial commit

---
+++ gen-list.py
@@ -0,0 +1,14 @@
+import os
+
+LIST_ITEM_TEMPLATE = """        <li class="list-item">
+          <h3>{nome}</h3>
+          <button type="button" onclick="playAudio(this, '{audio_file}')"/>
+        </li>
+"""
+
+for _, _, files in os.walk('./audios'):
+    for f in files:
+        if not f.endswith(".m4a"):
+            continue
+        
+        print(LIST_ITEM_TEMPLATE.format(nome=f[:-4], audio_file=f), end='')
