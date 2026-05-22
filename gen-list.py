1a973ef041746a456e12b62c320d744d50f895da
Arthur Coelho <arthurclho@gmail.com>
2026-05-22 13:39:37 -0300

    Fix gen-list for ogg audios

--- gen-list.py
+++ gen-list.py
@@ -8,7 +8,7 @@ LIST_ITEM_TEMPLATE = """        <li class="list-item">
 
 for _, _, files in os.walk('./audios'):
     for f in sorted(files):
-        if not f.endswith(".m4a"):
+        if not f.endswith(".ogg"):
             continue
         
         print(LIST_ITEM_TEMPLATE.format(nome=f[:-4], audio_file=f), end='')
